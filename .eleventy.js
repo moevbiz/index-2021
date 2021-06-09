c78ddb2b2bb5c9e21217a2ec902b26c0ae827495
Bruno <38008341+moevbiz@users.noreply.github.com>
2021-06-09 20:38:55 +0200

    cache buster

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,25 @@
+const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+
 module.exports = function(config) {
 
     config.addLayoutAlias('default', 'layouts/base.njk');
 
     config.addPassthroughCopy('./src/assets/images');
-
+    
     // ref the context
     let env = process.env.ELEVENTY_ENV;
-
+    
     // make the seed target act like prod
     env = (env=="seed") ? "prod" : env;
-  
+    
+    // cache buster
+    const cacheBusterOptions = {};
+
+    // only run cache buster in prod
+    if (env == 'prod') {
+        config.addPlugin(cacheBuster(cacheBusterOptions));
+    }
+
     // Base config
     return {
         passthroughFileCopy: true,
