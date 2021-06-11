1b6ddacc6664d2f8e24592738c499a03b2d7df65
Bruno <38008341+moevbiz@users.noreply.github.com>
2021-06-11 09:27:57 +0200

    add sub form

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,9 @@
-import { appHeight } from "./tools";
+import { appHeight, sub } from "./tools";
 
 window.addEventListener('resize', appHeight);
 appHeight();
+
+const nlForm = document.querySelector('.signup-form');
+nlForm.addEventListener('submit', (e) => {
+    sub(e);
+})
