2570dc83fb6ddeb17929dc549b11e818988a87a7
Bruno <38008341+moevbiz@users.noreply.github.com>
2021-06-15 14:11:48 +0200

    start developing api

--- src/_data/prod/spaces.js
+++ src/_data/prod/spaces.js
@@ -15,18 +15,16 @@ const seed = (data, path) => {
   }
 }
 
-const sheetID = "1XN9AyaFrvrVdw53PUFvCFSBElc0u35Eb4ZHnncdS91w";
-const sheet = {
-  sites: `https://spreadsheets.google.com/feeds/list/${sheetID}/1/public/values?alt=json`,
-  tech: `https://spreadsheets.google.com/feeds/list/${sheetID}/2/public/values?alt=json`,
+const sheetID = process.env.GOOGLE_SHEET_ID;
+const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/1/public/values?alt=json`;
+const indexAPI = 'https://independentspaceindex.at/spaces.json';
+
+function getIndex() {
+  return axios.get(indexAPI);
 }
 
-const getSites = () => {
-  return axios.get(sheet.sites);
-}
-
-const getTech = () => {
-  return axios.get(sheet.tech);
+function getParticipants() {
+  return axios.get(googleSheetUrl);
 }
 
 const yearDisplay = (item, years) => {
@@ -44,45 +42,11 @@ const yearDisplay = (item, years) => {
 module.exports = () => {
   return new Promise((resolve, reject) => {
 
-    Promise.all([getSites(), getTech()])
-      .then(([sites, tech]) => {
-        // massage the data from the Google Sheets API into
-        // a shape that will more convenient for us in our SSG.
-        var data = {
-          "content": [],
-          "types": [],
-          "tech": [],
-          "date": new Date('Y'),
-        };
-        let years = [];
-        tech.data.feed.entry.forEach(item => {
-          data.tech.push({
-            "name": item.gsx$tech.$t,
-            "color": item.gsx$color.$t
-          });
-        })
-        sites.data.feed.entry.forEach(item => {
-          data.content.push({
-            "year": item.gsx$year.$t,
-            "name": item.gsx$name.$t,
-            "link": item.gsx$url.$t,
-            "type": item.gsx$type.$t.split(', '),
-            "tech": item.gsx$tech.$t.split(', '),
-            "long": item.gsx$long.$t,
-            "mark": JSON.parse(item.gsx$highlight.$t.toLowerCase()),
-            "visible": JSON.parse(item.gsx$visible.$t.toLowerCase()),
-            "yearDisplay": yearDisplay(item, years)
-          })
-          if (JSON.parse(item.gsx$visible.$t.toLowerCase())) {
-            item.gsx$type.$t.split(', ').forEach(type => {
-              if (!data.types.includes(type)) {
-                data.types.push(type);
-              }
-            })
-          }
+    Promise.all([getIndex(), getParticipants()])
+      .then(([index, participants]) => {
+        participants.forEach((participant, i) => {
+          let spaceData = index.find(space => space.name === participant.gsx$space.$t)
         });
-
-        data.types.sort()
 
         // stash the data locally for developing without
         // needing to hit the API each time.
